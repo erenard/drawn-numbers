b20468737a9ec41533049c2fd67908f5983e25ff
Éric Renard <timmy469@gmail.com>
2020-05-23 14:13:28 +0200

    Numbers demonstrator

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -18,8 +18,26 @@ export default {
   methods: {
     handleClick () {
       framebuffer.begin()
-      numbers.drawNumberOne(100, 100, 32)
-      numbers.drawNumberOne(150, 100, 64)
+
+      for (let i = 0; i < 8; i++) {
+        numbers.drawNumber(i, 20 + i * 40, 80)
+      }
+      for (let i = 8; i < 10; i++) {
+        numbers.drawNumber(i, 20 + i * 40, 180)
+      }
+
+      for (let i = 0; i < 10; i++) {
+        numbers.drawNumber(i, 15 + i * 30, 240, 3)
+      }
+
+      for (let i = 0; i < 10; i++) {
+        numbers.drawNumber(i, 100 + i * 20, 180, 2)
+      }
+
+      for (let i = 0; i < 10; i++) {
+        numbers.drawNumber(i, 100 + i * 10, 120, 1)
+      }
+
       framebuffer.end()
     }
   }
